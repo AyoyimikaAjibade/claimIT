06612c292d2c63c454b78770a8c14690e0ab457b
Ayoyimika <ayoyimika@quickcheck.ng>
2025-05-04 09:13:18 -0700

    render deployment: config allowed host

--- claimIT/settings.py
+++ claimIT/settings.py
@@ -27,7 +27,17 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='claimit.onrender.com,claimit-backend-ss4k.onrender.com,localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='claimit.onrender.com,claimit-backend-ss4k.onrender.com,localhost,127.0.0.1',
+    cast=lambda v: [
+        s.strip()
+         .replace('http://', '')
+         .replace('https://', '')
+         .rstrip('/')
+        for s in v.split(',')
+    ]
+)
 
 
 # Application definition
